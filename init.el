aa610caa3d4849d944c9d81c1a0eb981634e9ef5
frosty <passedgoandgot200@gmail.com>
2024-07-04 15:54:50 -0400

    disable custom

--- init.el
+++ init.el
@@ -32,18 +32,5 @@
 (global-display-line-numbers-mode 1)
 (global-visual-line-mode t)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" default))
- '(package-selected-packages '(evil)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; Disable custom.el
+(setq custom-file null-device)
