db8e902526bf1c43139564e5b2efa674e9947d19
frosty <passedgoandgot200@gmail.com>
2024-09-06 05:08:40 -0400

    first try working at it

--- init.el
+++ init.el
@@ -1,36 +1,6 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(package-refresh-contents)
+;;; init.el --- Initialize configuration -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
 
-;; Download Evil
-(unless (package-installed-p 'evil)
-  (package-install 'evil))
-
-;; Enable Evil
-(require 'evil)
-(evil-mode 1)
-
-(unless (package-installed-p 'gruvbox-theme)
-  (package-install 'gruvbox-theme))
-
-(load-theme 'gruvbox-dark-medium)
-
-(set-face-attribute 'default nil
-  :font "Fira Mono"
-  :height 100
-  :weight 'medium)
-
-(set-face-attribute 'font-lock-comment-face nil
-  :slant 'italic)
-
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-
-(global-display-line-numbers-mode 1)
-(global-visual-line-mode t)
-
-;; Disable custom.el
-(setq custom-file null-device)
+(org-babel-load-file (locate-user-emacs-file "config.org"))
+;;; init.el ends here
